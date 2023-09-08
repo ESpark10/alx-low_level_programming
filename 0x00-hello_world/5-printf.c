424d27a041fac62994fb92f9c7340d2820b0b13a
Espark10 <olawuni74@gmail.com>
2023-09-07 19:29:48 -0700

    i was on absoltee firreee

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main -  this prints the input on it
- * return: always 0.
+ * Return: always 0
  */
 
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
