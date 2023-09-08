fa0da1da0b58b1645f47fc26e2bb8febf1dcb97e
Espark10 <olawuni74@gmail.com>
2023-09-07 19:24:10 -0700

    i was on absoltee firreee

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -  this will print the input on it
- * return: always 0
+ * main -  this will be the main function
+ * Return: always 0
  */
-	int main(void)
-	{
+	
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
