63facf0082a2cda7f52cd372185d8e13d5b4e0b7
Espark10 <olawuni74@gmail.com>
2023-09-10 08:04:43 -0700

    oya na make we roll

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+this is a comment
+main - entry point
+Return: 0 success
+*/
+
+int main()
+{
+
+    char x;
+    char y;
+        for (x = 'a'; x <= 'z'; x++)
+            {
+        putchar(x);
+            }
+        for (y = 'A'; y <= 'Z'; y++)
+        {
+            putchar(y);
+        }
+            putchar("\n");
+
+
+
+    return (0);
+}
