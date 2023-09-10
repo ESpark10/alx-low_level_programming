fd9d82837863304af061b3de6b1d588ecb77f880
Espark10 <olawuni74@gmail.com>
2023-09-10 09:50:03 -0700

    i am on fire sehh nibi bayi

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,32 @@
-wZZ
+#include <stdio.h>
+#include <stdlib.h>
+/**
+this is a comment
+main - entry point
+Return: 0 success
+*/
+
+int main()
+{
+
+   char x;
+
+   for (x = 'a'; x <= 'z'; x++)
+{
+
+        if (x == 'e')
+        {
+        continue;
+putchar(x);
+       }
+            if (x == 'q')
+        {
+        continue;
+        }
+putchar(x);
+
+}
+
+
+    return (0);
+}
