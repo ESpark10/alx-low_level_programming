6a8a59f78ae69a45ca6b0a7883453c7bc7237e44
Espark10 <olawuni74@gmail.com>
2023-09-11 08:37:43 -0700

    6,7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
-qcvby
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+this is a comment
+main - entry point
+Return: 0 success
+*/
+
+int main()
+
+{
+int x;
+
+   for (x = 0; x < 10; x++)
+
+    putchar(x + '0');
+    putchar('\n');
+
+}
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,21 @@
-dfgdhfb
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+this is a comment
+main - entry point
+Return: 0 success
+*/
+
+int main()
+{
+    char x;
+    for (x = 'z'; x >= 'a'; x--)
+        putchar(x);
+
+
+
+
+    return (0);
+}
+
