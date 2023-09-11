b606517bb90b22c7233b5a1fbc052b0f60313f84
Espark10 <olawuni74@gmail.com>
2023-09-11 09:08:43 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,26 +2,26 @@
 
 /**
 *this is a comment
-*main - entry point
+*main - this prints input
 *Return: 0 success
 */
 
 int main(void)
 {
 	char x;
-	
+
 	for (x = 'a'; x <= 'z'; x++)
 {
 		if (x == 'e')
-        {
+	{
 		continue;
 		x++;
 	}
 		if (x == 'q')
-        {
+	{
 		continue;
 		x++;
-        }
+	}
 putchar(x);
 
 }
