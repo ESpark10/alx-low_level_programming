5ef629eb24e3be63d734c50a408d292d7de5a127
Espark10 <olawuni74@gmail.com>
2023-09-11 19:51:09 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,13 +12,13 @@ int main(void)
 	char x;
 	char y;
 	for (x = 'a'; x <= 'z'; x++)
-	{
+		{
 		putchar(x);
-	}
+		}
 	for (y = 'A'; y <= 'Z'; y++)
-	{
+		{
 		putchar(y);
-	}
+		}
 	putchar('\n');
 
 	return (0);
