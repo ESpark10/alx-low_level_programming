246af6e49a1b36e51f44faea5e01c150755d150d
Espark10 <olawuni74@gmail.com>
2023-09-11 20:43:20 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-vgahssd
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - this prints the input
+*Return: 0 success
+*/
+
+
+int main()
+{
+	int basenum;
+		for (int basenum = 0; basenum < 16; basenum++)
+			{
+
+				char hexabase = (basenum < 10) ? ('0' + basenum) : ('a' + basenum - 10);
+
+				putchar(hexabase);
+
+			}
+		putchar('\n');
+	return (0);
+}
