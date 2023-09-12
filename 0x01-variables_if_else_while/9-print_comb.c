33691f182775167f9d7e421cd144ab20e5a4788e
Espark10 <olawuni74@gmail.com>
2023-09-11 20:25:41 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,18 @@
- qwqfomgmb
+#include <stdio.h>
+
+/**
+*main - this prints input straight
+*Return: 0 success
+*/
+
+int main()
+{
+int x;
+	for (x = 0; x <  10; x++)
+
+		putchar(x + '0');
+		putchar(", ");
+
+
+	return (0);
+}
