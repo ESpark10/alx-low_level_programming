79475f1262ca0dff0863808c91d744c3a205d23f
Espark10 <olawuni74@gmail.com>
2023-09-13 05:58:16 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+int _putchar(char c);
 
 
 /**
@@ -11,15 +10,15 @@
 int main(void)
 
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	
 	return (0);
 }
