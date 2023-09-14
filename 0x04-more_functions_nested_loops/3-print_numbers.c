0e409de308e7212b8be4254dc507cc068d3bc039
Espark10 <olawuni74@gmail.com>
2023-09-14 11:39:07 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for lowercase letters
+ *
+ * @c: (char) The character to be checked.
+ *
+ * Return: 1 if the statement is true while 0 if otherwise
+ */
+
+void print_numbers(void)
+{
+	for (x = 0; x <= 9; x++)
+		_putchar(x);
+	_putchar('\n');
+}
+
