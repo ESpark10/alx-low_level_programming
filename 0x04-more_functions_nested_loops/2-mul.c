c406d9c0a603b8daa8bdb92d0bd0814a6f49e976
Espark10 <olawuni74@gmail.com>
2023-09-14 06:52:47 -0700

    2-main.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,11 @@
 /**
  * mul - check for digit and multiply
  *
- * @c: (char) The character to be checked.
- *
  * Return: 1 if the statement is true while 0 if otherwise
  */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
