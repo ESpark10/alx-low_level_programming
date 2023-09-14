6dc71f71638cc2a0cab754777131c7eba06e95b3
Espark10 <olawuni74@gmail.com>
2023-09-14 13:23:46 -0700

    4-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,11 @@ void print_most_numbers(void)
 	for (x = 0; x <= 9; x++)
 	{
 		if (x == 2 || x == 4)
+		{
 			continue;
-		x++;
-	_putchar(x + '0');
+			x++;
+		}
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
