cce51ac9e8a7b7caa7319a9797a503c5bc6b4c5e
Espark10 <olawuni74@gmail.com>
2023-09-14 06:48:24 -0700

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check for digit and multiply
+ *
+ * @c: (char) The character to be checked.
+ *
+ * Return: 1 if the statement is true while 0 if otherwise
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
