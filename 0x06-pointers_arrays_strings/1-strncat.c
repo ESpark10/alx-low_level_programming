15f7b4ed1fc1dffb86dee5e6eeef18c92bf7287e
Espark10 <olawuni74@gmail.com>
2023-09-20 18:01:00 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,30 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * _strncat - concatenate two strings
+ * @dest: prints input
+ *@src: sends input
+ *@n: increases
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *mydest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = ('\0');
+
+	return (mydest);
+}
