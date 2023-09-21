8fbc9980254f8c4f3f3bf6ad789b759703e4dd63
Espark10 <olawuni74@gmail.com>
2023-09-20 18:52:41 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,27 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+*reverse_array- this reverses the input
+ *@a: prints input
+ *@n: sends input
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
+
+void reverse_array(int *a, int n)
+{
+	int x = 0;
+
+	int y = n - 1;
+
+	while (x < y)
+	{
+		int temp = a[x];
+
+		a[x] = a[y];
+		a[y] = temp;
+
+		x++;
+		y--;
+	}
+}
